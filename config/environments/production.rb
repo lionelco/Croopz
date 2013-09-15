b0edf679fd02707f7c7cd93efb4e20c5e823bf7e
Lionel <lionel.comandini@gmail.com>
2013-09-16 00:06:24 +0200

    host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ SampleApp::Application.configure do
 config.action_mailer.perform_deliveries = true # Set it to false to disable the email in dev mode
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
-config.action_mailer.default_url_options = { :host => "smtp.gmail.com" }
+config.action_mailer.default_url_options = { :host => "croopz.com" }
 
 
 ActionMailer::Base.smtp_settings = {
